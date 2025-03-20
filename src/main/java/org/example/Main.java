967d7dca258b28b368f360eed496d3b9c8497773
tmfajddl <tmfajddl12@gmail.com>
2025-03-20 15:27:05 +0900

    1번 문제

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("가로의 길이를 입력하세요: ");
+        int w = sc.nextInt(); // 가로 길이 대입
+        System.out.printf("세로의 길이를 입력하세요: ");
+        int h = sc.nextInt(); //세로 길이 대입
+        for(int i = 0; i < h; i++){ //줄 수 반복
+            for(int j = 0; j < w; j++) // 한줄에 찍히는 별 반복
+                System.out.print("*");
+            System.out.println(); // 줄바꿈 표시
+        }
+    }
+}
