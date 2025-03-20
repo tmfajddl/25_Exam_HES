e938516407ea59503ca7cc0e3d6485299cb46848
tmfajddl <tmfajddl12@gmail.com>
2025-03-20 15:42:25 +0900

    1번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("양수의 정수를 입력하시오: ");
-        int n = sc.nextInt(); // 정수 크기 대입
-        if(n <= 0) // 양의 정수가 아닐경우 출력문 출력
-            System.out.println("양수가 아닙니다.");
-        for(int i = 0; i < n; i++){//줄 수 반복
-            for(int j = 0; j < i; j++) // 공백 출력
-                System.out.print(" ");
-            for(int j = n-i; j >= 1; j--) // n-i 만큼의 수 출력
-                System.out.printf("%d ",j);
-            System.out.println(); // 줄바꿈 표시
+        System.out.printf("가로의 길이를 입력하세요: ");
+        int w = sc.nextInt(); // 가로 길이 대입
+        System.out.printf("세로의 길이를 입력하세요: ");
+        int h = sc.nextInt(); //세로 길이 대입
+        for(int i = 0; i < h; i++){ //줄 수 반복
+            for(int j = 0; j < w; j++) // 한줄에 찍히는 별 반복
+                System.out.print("*");
+            System.out.println(); // 줄바꿈
         }
     }
 }
