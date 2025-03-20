e2981ec1f687067eef72e2b82d28a86069eb18df
tmfajddl <tmfajddl12@gmail.com>
2025-03-20 15:57:23 +0900

    3번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("가로의 길이를 입력하세요: ");
-        int w = sc.nextInt(); // 가로 길이 대입
-        System.out.printf("세로의 길이를 입력하세요: ");
-        int h = sc.nextInt(); //세로 길이 대입
-        for(int i = 0; i < h; i++){ //줄 수 반복
-            for(int j = 0; j < w; j++) // 한줄에 찍히는 별 반복
-                System.out.print("*");
-            System.out.println(); // 줄바꿈
+        int[] arr = new int[10]; // 10개인 배열 생성
+        for(int i = 0; i < arr.length; i++) //배열에 각각의 값 입력
+            arr[i] = i;
+        for(int i = 0; i < arr.length; i++){ // 값 치환
+            if(i < 2)
+                continue;
+            arr[i] = arr[i-2] + arr[i-1];
         }
+        for(int i = 0; i < arr.length; i++) //출력
+            System.out.print(arr[i] + " ");
     }
 }
